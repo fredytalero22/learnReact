5a642a1109e3ead86747b29e32bc26816afb852d
fredy.talero <fredy.talero@sabatech.com.co>
2020-02-05 18:30:06 -0500

    Incluir la barra de navegacion

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-import ListaInmuebles from './componentes/vistas/ListaInmuebles';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import theme from './theme/theme';
+import React, { Component } from "react";
+import "./App.css";
+import ListaInmuebles from "./componentes/vistas/ListaInmuebles";
+import AppNavbar from "./componentes/layout/AppNavbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {MuiThemeProvider} from "@material-ui/core/styles";
+import theme from "./theme/theme";
+import Grid from "@material-ui/core/Grid";
 
-class App extends Component{
-  render(){
-    return(
-      <MuiThemeProvider theme={theme}>
-        <ListaInmuebles />
-      </MuiThemeProvider>
-      
-    )
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <MuiThemeProvider theme={theme}>
+          <AppNavbar />
+          <Grid container>
+            <Switch>
+              <Route path="/" exact component={ListaInmuebles}></Route>
+            </Switch>
+          </Grid>
+        </MuiThemeProvider>
+      </Router>
+    );
   }
 }
 
